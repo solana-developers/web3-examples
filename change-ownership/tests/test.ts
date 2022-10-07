6f92beb9bcdaa4a87f16a976fe91cce20b3728cd
jpcaulfi <joe.caulfield@solana.org>
2022-10-07 08:54:16 -0400

    change ownership example

---
+++ change-ownership/tests/test.ts
@@ -0,0 +1,79 @@
+import {
+    Connection,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    sendAndConfirmTransaction,
+    SystemProgram,
+    Transaction,
+} from '@solana/web3.js';
+
+
+function createKeypairFromFile(path: string): Keypair {
+    return Keypair.fromSecretKey(
+        Buffer.from(JSON.parse(require('fs').readFileSync(path, "utf-8")))
+    )
+};
+
+
+describe("Change an account's owner", async () => {
+
+    const connection = new Connection(`https://api.devnet.solana.com`, 'confirmed');
+    const payer = createKeypairFromFile(require('os').homedir() + '/.config/solana/id.json');
+    
+    const PROGRAM_ID: PublicKey = new PublicKey(
+        "Au21huMZuDQrbzu2Ec5ohpW5CKRqhcGV6qLawfydStGs"
+    ); // This is just an arbitrary Program ID for example
+
+    const newKeypair = Keypair.generate();
+  
+    it("Create the account", async () => {
+
+        let ix = SystemProgram.createAccount({
+            fromPubkey: payer.publicKey,
+            newAccountPubkey: newKeypair.publicKey,
+            lamports: 1 * LAMPORTS_PER_SOL,
+            space: 0,
+            programId: SystemProgram.programId,
+        });
+
+        await sendAndConfirmTransaction(
+            connection, 
+            new Transaction().add(ix),
+            [payer, newKeypair]
+        );
+    });
+
+    it("Change ownership for the account", async () => {
+
+        const newKeypair = Keypair.generate();
+
+        let ix = SystemProgram.assign({
+            accountPubkey: newKeypair.publicKey,
+            programId: PROGRAM_ID
+        })
+
+        await sendAndConfirmTransaction(
+            connection, 
+            new Transaction().add(ix),
+            [payer, newKeypair]
+        );
+    });
+
+    it("Change it again using the System Program", async () => {
+
+        const newKeypair = Keypair.generate();
+
+        let ix = SystemProgram.assign({
+            accountPubkey: newKeypair.publicKey,
+            programId: SystemProgram.programId,
+        })
+
+        await sendAndConfirmTransaction(
+            connection, 
+            new Transaction().add(ix),
+            [payer, newKeypair]
+        );
+    });
+  });
+  
