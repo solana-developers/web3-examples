eeb1e31805bf3ea27cfa9db0225c6b5fa39bf754
Valentin Madrid <valentin@incility.com>
2023-03-14 21:30:41 +0100

    🍉 - Add Transfer-fees and Bearing-mints examples

---
+++ tokens/token-2022/interest-bearing/src/main.ts
@@ -0,0 +1,63 @@
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import {
+  createInterestBearingMint,
+  updateRateInterestBearingMint,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+
+(async () => {
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  const payer = Keypair.generate();
+  const airdropSignature = await connection.requestAirdrop(
+    payer.publicKey,
+    2 * LAMPORTS_PER_SOL
+  );
+  await connection.confirmTransaction({
+    signature: airdropSignature,
+    ...(await connection.getLatestBlockhash()),
+  });
+
+  const mintAuthority = Keypair.generate();
+  const freezeAuthority = Keypair.generate();
+  const rateAuthority = Keypair.generate();
+  const mintKeypair = Keypair.generate();
+  const rate = 10;
+  const decimals = 9;
+  const mint = await createInterestBearingMint(
+    connection,
+    payer,
+    mintAuthority.publicKey,
+    freezeAuthority.publicKey,
+    rateAuthority.publicKey,
+    rate,
+    decimals,
+    mintKeypair,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  // Update rate
+  const updatedRate = 50;
+  const updatedMint = await updateRateInterestBearingMint(
+    connection,
+    payer,
+    mint,
+    rateAuthority,
+    updatedRate,
+    [],
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+  console.log(
+    "Your transaction signature for updating the Token Rate",
+    updatedMint
+  );
+
+  console.log("Your Token mint PublicKey", mintKeypair.publicKey.toBase58());
+})();
---
+++ tokens/token-2022/transfer-fee/src/main.ts
@@ -0,0 +1,90 @@
+import {
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+  Connection,
+  Keypair,
+  SystemProgram,
+  Transaction,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+
+import {
+  ExtensionType,
+  createInitializeMintInstruction,
+  mintTo,
+  createAccount,
+  getMintLen,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+
+import {
+  createInitializeTransferFeeConfigInstruction,
+  harvestWithheldTokensToMint,
+  transferCheckedWithFee,
+  withdrawWithheldTokensFromAccounts,
+  withdrawWithheldTokensFromMint,
+} from "@solana/spl-token";
+
+(async () => {
+  const payer = Keypair.generate();
+
+  const mintAuthority = Keypair.generate();
+  const mintKeypair = Keypair.generate();
+  const mint = mintKeypair.publicKey;
+  const transferFeeConfigAuthority = Keypair.generate();
+  const withdrawWithheldAuthority = Keypair.generate();
+
+  const extensions = [ExtensionType.TransferFeeConfig];
+
+  const mintLen = getMintLen(extensions);
+  const decimals = 9;
+  const feeBasisPoints = 50;
+  const maxFee = BigInt(5_000);
+
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  const airdropSignature = await connection.requestAirdrop(
+    payer.publicKey,
+    2 * LAMPORTS_PER_SOL
+  );
+  await connection.confirmTransaction({
+    signature: airdropSignature,
+    ...(await connection.getLatestBlockhash()),
+  });
+
+  const mintLamports = await connection.getMinimumBalanceForRentExemption(
+    mintLen
+  );
+  const mintTransaction = new Transaction().add(
+    SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: mint,
+      space: mintLen,
+      lamports: mintLamports,
+      programId: TOKEN_2022_PROGRAM_ID,
+    }),
+    createInitializeTransferFeeConfigInstruction(
+      mint,
+      transferFeeConfigAuthority.publicKey,
+      withdrawWithheldAuthority.publicKey,
+      feeBasisPoints,
+      maxFee,
+      TOKEN_2022_PROGRAM_ID
+    ),
+    createInitializeMintInstruction(
+      mint,
+      decimals,
+      mintAuthority.publicKey,
+      null,
+      TOKEN_2022_PROGRAM_ID
+    )
+  );
+  const transaction = await sendAndConfirmTransaction(
+    connection,
+    mintTransaction,
+    [payer, mintKeypair],
+    undefined
+  );
+  console.log("Your transaction PublicKey:", transaction);
+  console.log("Your mint PublicKey:", mint.toBase58());
+})();
