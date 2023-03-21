900dca6eda9e7ba7c801023891c446def11da72c
shivam <shivam.spirite3@outlook.com>
2023-03-21 22:58:21 +0530

    add non-transferable-token-example

---
+++ tokens/token-2022/non-transferable-token/src/main.ts
@@ -0,0 +1,49 @@
+import {
+    clusterApiUrl,
+    sendAndConfirmTransaction,
+    Connection,
+    Keypair,
+    SystemProgram,
+    Transaction,
+    LAMPORTS_PER_SOL,
+} from '@solana/web3.js';
+import {
+    createInitializeNonTransferableMintInstruction,
+    createInitializeMintInstruction,
+    getMintLen,
+    ExtensionType,
+    TOKEN_2022_PROGRAM_ID,
+} from '@solana/spl-token';
+
+(async () => {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+    const payer = Keypair.generate();
+    const airdropSignature = await connection.requestAirdrop(payer.publicKey, 2 * LAMPORTS_PER_SOL);
+    await connection.confirmTransaction({ signature: airdropSignature, ...(await connection.getLatestBlockhash()) });
+
+    const mintAuthority = Keypair.generate();
+    const decimals = 9;
+
+    const mintKeypair = Keypair.generate();
+    const mint = mintKeypair.publicKey;
+
+    // for one time issuing token use this then frezze this or burn this
+    const mintLen = getMintLen([ExtensionType.NonTransferable]);
+
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen);
+
+    const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: payer.publicKey,
+            newAccountPubkey: mint,
+            space: mintLen,
+            lamports,
+            programId: TOKEN_2022_PROGRAM_ID,
+        }),
+        createInitializeNonTransferableMintInstruction(mint, TOKEN_2022_PROGRAM_ID),
+        createInitializeMintInstruction(mint, decimals, mintAuthority.publicKey, null, TOKEN_2022_PROGRAM_ID)
+    );
+    let signature = await sendAndConfirmTransaction(connection, transaction, [payer, mintKeypair], undefined);
+    console.log('signature', signature)
+})();
