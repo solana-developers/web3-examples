765abea86c9a8f361dff419e6ac547c79a5d2128
shivam <shivam.spirite3@outlook.com>
2023-06-03 11:10:44 +0530

    update clusterUri

--- tokens/wrapping-sol/src/main.ts
+++ tokens/wrapping-sol/src/main.ts
@@ -2,13 +2,10 @@ import { NATIVE_MINT, createAssociatedTokenAccountInstruction, getAssociatedToke
 import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
 
 (async () => {
-const connection = new Connection(
-  "https://api.devnet.solana.com", 
-  {
-      commitment: 'confirmed',
-      confirmTransactionInitialTimeout: 60000,
-  },
-)
+  const connection = new Connection(
+    clusterApiUrl('devnet'),
+    'confirmed' 
+  )
   const wallet = Keypair.generate();
 
   const airdropSignature = await connection.requestAirdrop(
@@ -50,11 +47,11 @@ const connection = new Connection(
     )
 
   const tx = await sendAndConfirmTransaction(connection, solTransferTransaction, [wallet]);
-  console.log(`tx signature:",${tx}`)
+  console.log(`tx signature: ${tx}`)
   const accountInfo = await getAccount(connection, associatedTokenAccount);
   console.log(`Native: ${accountInfo.isNative}, Lamports: ${accountInfo.amount}`);
   const walletBalance = await connection.getBalance(wallet.publicKey);
-  console.log(`Balance before unwrapping 1 WSOL:",${walletBalance}`)
+  console.log(`Balance before unwrapping 1 WSOL: ${walletBalance}`)
 
   // retrieving WSOL and send tokens to wallet
   await closeAccount(connection, wallet, associatedTokenAccount, wallet.publicKey, wallet);
